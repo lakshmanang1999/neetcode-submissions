d6ecdee3a42ea37813bd3a6fc2a83a58a1960cfe
lakshmanang1999 <58934325+lakshmanang1999@users.noreply.github.com>
2026-08-25 07:19:33 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    // isAnagram(s, t) {
+    //     if(s.length !== t.length) return false;
+    //     const hash = {}
+    //     for(let value of s){
+    //         hash[value] = (hash[value] ?? 0) + 1
+    //     }
+    //     for(let value of t){
+    //         hash[value] = (hash[value] ?? 0) - 1
+    //     }
+        
+    //     for(let key in hash){
+    //         const value = hash[key]
+    //         if(value !== 0){
+    //             return false
+    //         }
+    //     }
+    //     return true
+    // }
+    // isAnagram(s, t) {
+    //     if(s.length !== t.length) return false;
+    //     const sSort = s.split("").sort().join("")
+    //     const tSort = t.split("").sort().join("");
+    //     return sSort === tSort
+    // }
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        const hash = {}
+        for(let i =0; i<s.length; i++){
+            hash[s[i]] = (hash[s[i]] ?? 0) + 1
+            hash[t[i]] = (hash[t[i]] ?? 0) - 1
+        }        
+        for(let key in hash){
+            const value = hash[key]
+            if(value !== 0){
+                return false
+            }
+        }
+        return true
+    }
+}
