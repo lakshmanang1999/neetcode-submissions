e0daa547fa6eb51c0d1964dc3e2989ed68fe9cda
lakshmanang1999 <58934325+lakshmanang1999@users.noreply.github.com>
2026-08-25 07:02:00 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    // hasDuplicate(nums: number[]): boolean {
+    //     const hash = {};
+    //     for(let i of nums){
+    //         if(hash.hasOwnProperty(i)){
+    //             return true;
+    //         }else {
+    //             hash[i] = hash[i] + 1
+    //         }
+    //     }
+    //     return false
+    // }
+    hasDuplicate(nums: number[]): boolean {
+        const unique = new Set(nums);
+        return unique.size < nums.length
+        
+    }
+}
