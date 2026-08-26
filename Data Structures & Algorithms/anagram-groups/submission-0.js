0e5ceddb36910399cfc657a8ad8214bccfd1a190
lakshmanang1999 <58934325+lakshmanang1999@users.noreply.github.com>
2026-08-26 07:12:58 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    isAnagram(s, t) {
+       if(s.length !== t.length) return false;
+       const hash = {};
+       for(let i=0;i<s.length;i++){
+        hash[s[i]] = (hash[s[i]] ?? 0) - 1
+        hash[t[i]] = (hash[t[i]] ?? 0) - 1
+       }
+       for(let key in hash){
+        const value = hash[key]
+            if(value !== 0){
+                return false
+            }
+       }
+       return true;
+    }
+    groupAnagrams(strs) {
+        const obj = {};
+        for(let i=0;i<strs.length;i++){
+            const value = strs[i]
+            const sValue = strs[i].split("").sort().join("")
+            if(obj.hasOwnProperty(sValue)){
+                obj[sValue].push(value);
+                continue;
+            }
+            obj[sValue] = [value]
+        }
+        const result = []
+        for(let key in obj){
+            result.push(obj[key])
+        }
+        return result
+    }
+}
