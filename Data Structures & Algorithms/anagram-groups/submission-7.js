f542cfe0bbbe31d099e0c9a45d449caa11ba4192
lakshmanang1999 <58934325+lakshmanang1999@users.noreply.github.com>
2026-08-26 07:42:01 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    
+    // groupAnagrams(strs) {
+    //     const obj = {};
+    //     for(let i=0;i<strs.length;i++){
+    //         const value = strs[i]
+    //         const sValue = strs[i].split("").sort().join("")
+    //         if(obj.hasOwnProperty(sValue)){
+    //             obj[sValue].push(value);
+    //             continue;
+    //         }
+    //         obj[sValue] = [value]
+    //     }
+    //     return Object.values(obj)
+    // }
+    groupAnagrams(strs) {
+        const obj = {};
+        for(let i=0;i<strs.length;i++){
+            const count = Array(26).fill(0);
+            const value = strs[i];
+            for(let v of value){
+                const idx = "z".charCodeAt(0) - v.charCodeAt(0);
+                count[idx] = count[idx] + 1
+            }
+            const groupKey = count.join(",");
+            if(obj[groupKey]){
+                obj[groupKey].push(value)
+            }else {
+                obj[groupKey] = [value]
+            }
+            
+        }
+        return Object.values(obj)
+    }
+}
